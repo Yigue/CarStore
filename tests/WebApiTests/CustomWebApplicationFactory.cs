4f60d02fb7abd05a4702acc8484a1cde159b215e
Guillermo Riedel <118210723+Yigue@users.noreply.github.com>
2025-09-29 18:33:34 -0300

    Add Web API integration tests

---
+++ tests/WebApiTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,37 @@
+using Application.Abstractions.Data;
+using Infrastructure.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Web.Api;
+
+namespace WebApiTests;
+
+public sealed class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly string _dbName = Guid.NewGuid().ToString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+            services.RemoveAll(typeof(ApplicationDbContext));
+            services.RemoveAll(typeof(IApplicationDbContext));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase(_dbName));
+
+            services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+}
